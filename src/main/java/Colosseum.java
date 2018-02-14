78d6d44aada0f194b916f6c0c21276a920e175c8
pinnw2 <pinnw2@illinois.edu>
2018-02-14 16:26:35 -0600

    Finished!

--- src/main/java/Colosseum.java
+++ src/main/java/Colosseum.java
@@ -73,6 +73,25 @@ public class Colosseum {
      */
     public static Pokemon buildPokemon() {
         Pokemon tempPokemon = new Pokemon();
+        System.out.println("Please enter name");
+        tempPokemon.name = myScan.nextLine();
+        System.out.println("Please enter hitpoint");
+        tempPokemon.hitPoints = myScan.nextInt();
+        System.out.println("Please enter attack level");
+        tempPokemon.attackLevel = myScan.nextInt();
+        System.out.println("Please enter defense level");
+        tempPokemon.defenseLevel = myScan.nextInt();
+
+        int hitPoints = -1;
+        int attackLevel = -1;
+        int defenseLevel = -1;
+        if (1 < hitPoints || hitPoints < MAX_HIT_POINTS) {
+            if (1 < attackLevel || attackLevel < 49){
+                if (1 < defenseLevel || defenseLevel < 23){
+                }
+            }
+        }
+        myScan.nextLine();
         return tempPokemon;
     }
 
@@ -90,7 +109,12 @@ public class Colosseum {
      * Implement this function.
      */
     public static void printWhoIsAhead() {
-        System.out.println("Implement me!");
+
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints){
+            System.out.println( firstPokemon.name + " is currently ahead!");
+        } else {
+            System.out.println( secondPokemon.name + " is currently ahead!");
+        }
     }
 
     /**
@@ -101,7 +125,11 @@ public class Colosseum {
      * Write this function.
      */
     public static void determineWinner() {
-        System.out.println("Implement me!");
+        if (firstPokemon.hitPoints > secondPokemon.hitPoints){
+            System.out.println( firstPokemon.name + " is the winner!");
+        } else {
+            System.out.println( secondPokemon.name + " is the winner!");
+        }
     }
 
     /**
@@ -110,17 +138,17 @@ public class Colosseum {
      * You do not need to modify this function.
      */
     public static void initializePokemon() {
-        System.out.println("Player 1, build your Pokemon!");
-        System.out.println("=================");
+        //*System.out.println("Player 1, build your Pokemon!");
+        //*System.out.println("=================");
         firstPokemon = buildPokemon();
-        firstPokemon.name = "Chuchu";
+        //*firstPokemon.name = "Chuchu";
 
         System.out.println("");
 
-        System.out.println("Player 2, build your Pokemon!");
-        System.out.println("==================");
+        //*System.out.println("Player 2, build your Pokemon!");
+        //* System.out.println("==================");
         secondPokemon = buildPokemon();
-        secondPokemon.name = "Xyz";
+        //*secondPokemon.name = "Xyz";
     }
 
     /**
